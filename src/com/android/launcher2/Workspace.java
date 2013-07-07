fe5de7ecdab752902cb9d1401f2dff0a34ef4801
lenny_kano <lennykano@gmail.com>
2013-07-07 20:09:47 +0930

    Fix couple of slipups

--- src/com/android/launcher2/Workspace.java
+++ src/com/android/launcher2/Workspace.java
@@ -855,18 +855,19 @@ public class Workspace extends SmoothPagedView
     }
 
     protected void setWallpaperDimension() {
+        int maxDim, minDim;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             Point minDims = new Point();
             Point maxDims = new Point();
             mLauncher.getWindowManager().getDefaultDisplay().getCurrentSizeRange(minDims, maxDims);
 
-            final int maxDim = Math.max(maxDims.x, maxDims.y);
-            final int minDim = Math.min(minDims.x, minDims.y);
+            maxDim = Math.max(maxDims.x, maxDims.y);
+            minDim = Math.min(minDims.x, minDims.y);
         } else {
             DisplayMetrics displayMetrics = new DisplayMetrics();
             mLauncher.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
-            final int maxDim = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
-            final int minDim = Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels);
+            maxDim = Math.max(displayMetrics.widthPixels, displayMetrics.heightPixels);
+            minDim = Math.min(displayMetrics.widthPixels, displayMetrics.heightPixels);
         }
 
         // We need to ensure that there is enough extra space in the wallpaper for the intended
