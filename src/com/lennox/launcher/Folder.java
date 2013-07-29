018b31b7940e7bd6844d819abdc31e19e3f97529
lenny_kano <lennykano@gmail.com>
2013-07-29 14:27:39 +0930

    Missed this

--- src/com/lennox/launcher/Folder.java
+++ src/com/lennox/launcher/Folder.java
@@ -147,6 +147,8 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
 
         mIsLandscape = LauncherApplication.isScreenLandscape(context);
         mIconScale = (float) PreferencesProvider.Interface.Homescreen.FolderIconStyle.getIconScale(mIsLandscape) / 100f;
+        mTextScale = (float) PreferencesProvider.Interface.Homescreen.FolderIconStyle.getTextScale(mIsLandscape) / 100f;
+        mTextPadding = PreferencesProvider.Interface.Homescreen.FolderIconStyle.getTextPadding(mIsLandscape);
         mInputMethodManager = (InputMethodManager)
                 getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
 
